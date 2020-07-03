2f7c21bf0fa9ee19263ade00e501e61f2c1c463e
Jeong minkyo <jeong.minkyo@nbt.com>
2020-07-03 19:15:27 +0900

    todo title 추가

---
+++ app/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View
+} from 'react-native';
+import Heading from './Heading'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All'
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView keyboardShouldPersistTaps='always'
+          style={styles.content}>
+            <Heading />
+          </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5'
+  },
+
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
+export default App
